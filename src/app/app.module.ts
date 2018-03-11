5dd035ced01e12d022d2fde7f7b0352f208ef8a5
camilla <camilladealmeidaalves@gmail.com>
2018-03-11 17:03:38 -0300

    Commit atividade 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PostsComponent } from './posts/posts.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule
